2ccad2362e00df297e0a16fdea08237d10bbb4e3
izhddm <5724724@mail.ru>
2024-04-19 15:12:00 +0400

    chore: документирование DatabaseInterface.php

--- Interface/DatabaseInterface.php
+++ Interface/DatabaseInterface.php
@@ -4,7 +4,19 @@ namespace FpDbTest\Interface;
 
 interface DatabaseInterface
 {
+    /**
+     * Строит конечный запрос, заменяя места вставки (?) конечными значениями, а также обрабатывает условные блоки ({...}).
+     *
+     * @param string $query Исходный запрос.
+     * @param array  $args  Аргументы для замены.
+     *
+     * @return string Конечный запрос.
+     */
     public function buildQuery(string $query, array $args = []): string;
 
+    /**
+     * Получает значение, указывающее на пропуск условного блока.
+     *
+     */
     public function skip();
 }
