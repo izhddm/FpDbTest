5e233f76b74df9741c798daf02306fee6ad002fd
izhddm <5724724@mail.ru>
2024-04-20 17:11:50 +0400

    fix: неверная проверка на тип в BooleanHandler.php

--- Handler/BooleanHandler.php
+++ Handler/BooleanHandler.php
@@ -9,8 +9,8 @@ class BooleanHandler implements ParameterHandlerInterface
 {
     public function handle($value): int
     {
-        if (!is_array($value)) {
-            throw new InvalidArgumentException("Expected Boolean, got ".gettype($value));
+        if (!is_bool($value)) {
+            throw new InvalidArgumentException('Expected Boolean, got '.gettype($value));
         }
 
         return (int)$value;
