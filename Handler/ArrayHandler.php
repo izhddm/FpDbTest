6ba0ee1f030ec86c398c4e91e0e1acf245879881
izhddm <5724724@mail.ru>
2024-04-20 17:51:16 +0400

    chore: убрана избыточная функция проверки массива на ассоциативность в ArrayHandler.php

--- Handler/ArrayHandler.php
+++ Handler/ArrayHandler.php
@@ -19,29 +19,17 @@ class ArrayHandler implements ParameterHandlerInterface, MysqliRequiredInterface
             throw new InvalidArgumentException('Expected Array, got '.gettype($value));
         }
 
-        $isAssociativeArray = $this->isAssociativeArray($value);
+        $isList = array_is_list($value);
         $result = [];
 
         foreach ($value as $key => $v) {
-            if ($isAssociativeArray) {
-                $result[] = (new IdentifierHandler())->handle($key).' = '.(new DefaultHandler($this->mysqli))->handle($v);
-            } else {
+            if ($isList) {
                 $result[] = (new DefaultHandler($this->mysqli))->handle($v);
+            } else {
+                $result[] = (new IdentifierHandler())->handle($key).' = '.(new DefaultHandler($this->mysqli))->handle($v);
             }
         }
 
         return implode(', ', $result);
-    }
-
-    private function isAssociativeArray(array $array): bool
-    {
-        $expectedKey = 0;
-        foreach ($array as $key => $value) {
-            if ($key !== $expectedKey++) {
-                return true; // Если хотя бы один ключ не соответствует ожидаемому, массив ассоциативный
-            }
-        }
-
-        return false; // Если все ключи соответствуют ожидаемым значениям, массив не ассоциативный
     }
 }
