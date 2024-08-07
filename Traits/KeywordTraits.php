9cd928434c794af473b0f991b0d1929bc815b9a8
izhddm <5724724@mail.ru>
2024-08-08 03:13:35 +0400

    feat: трайт с возможными ключевыми словами sql запроса

---
+++ Traits/KeywordTraits.php
@@ -0,0 +1,25 @@
+<?php
+
+trait KeywordTraits
+{
+    private array $keywords
+        = [
+            'SELECT',
+            'INSERT',
+            'UPDATE',
+            'DELETE',
+            'FROM',
+            'WHERE',
+            'VALUES',
+            'SET',
+            'ORDER',
+            'BY',
+            'LIMIT',
+            'INTO',
+            'JOIN',
+            'ON',
+            'GROUP',
+            'HAVING',
+            'AS',
+        ];
+}
