cae2e32a200eaa5d73a6e5deabf8aa2da986b7c7
izhddm <5724724@mail.ru>
2024-08-08 11:16:26 +0400

    feat: общий класс работы с query строкой

--- Database.php
+++ Database.php
@@ -2,7 +2,13 @@
 
 namespace FpDbTest;
 
+use FpDbTest\Factory\ParameterHandlerFactory;
+use FpDbTest\Interface\AstBuilderInterface;
+use FpDbTest\Interface\CommentRemoverInterface;
 use FpDbTest\Interface\DatabaseInterface;
+use FpDbTest\Interface\ModifierReplacerInterface;
+use FpDbTest\Interface\SqlBuilderInterface;
+use FpDbTest\Interface\TokenizerInterface;
 use mysqli;
 
 /**
@@ -12,19 +18,33 @@ class Database implements DatabaseInterface
 {
     private const float SKIP = INF;
 
+    private CommentRemoverInterface $commentRemover;
+    private TokenizerInterface $tokenizer;
+    private AstBuilderInterface $astBuilder;
+    private SqlBuilderInterface $sqlBuilder;
+    private ModifierReplacerInterface $modifierReplacer;
+
     public function __construct(protected mysqli $mysqli)
     {
+        $this->commentRemover = new CommentRemover();
+        $this->tokenizer = new Tokenizer();
+        $this->astBuilder = new AstBuilder();
+        $this->sqlBuilder = new SqlBuilder();
+        $this->modifierReplacer = new ModifierReplacer(new ParameterHandlerFactory());
     }
 
     public function buildQuery(string $query, array $args = []): string|false
     {
-        if (QueryValidator::validate($query, $args)) {
-            list($normalizedQuery, $normalizedArgs) = QueryNormalizer::normalize($query, $args);
+        $sqlParser = new SQLParser($this->commentRemover, $this->tokenizer, $this->astBuilder, $this->sqlBuilder, $this->modifierReplacer);
 
-            return QueryParser::parse($normalizedQuery, $this->skip(), $this->mysqli, $normalizedArgs);
-        }
+        // Преобразуем sql запрос в дерево
+        $ast = $sqlParser->parse($query);
 
-        return false;
+        // Заменяем модификаторы на значения
+        $ast = $sqlParser->replaceModifiers($ast, $args, $this->mysqli);
+
+        // Преобразуем дерево в sql запрос и возвращаем
+        return $sqlParser->toSQL($ast);
     }
 
     public function skip(): float
