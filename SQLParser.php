9eaa5005e06875baef8653f5e89363e2e8a95f25
izhddm <5724724@mail.ru>
2024-08-08 11:15:39 +0400

    feat: общий класс работы с query строкой

--- SQLParser.php
+++ SQLParser.php
@@ -2,10 +2,13 @@
 
 namespace FpDbTest;
 
+use FpDbTest\Factory\ParameterHandlerFactory;
 use FpDbTest\Interface\AstBuilderInterface;
 use FpDbTest\Interface\CommentRemoverInterface;
+use FpDbTest\Interface\ModifierReplacerInterface;
 use FpDbTest\Interface\SqlBuilderInterface;
 use FpDbTest\Interface\TokenizerInterface;
+use mysqli;
 
 class SQLParser
 {
@@ -13,17 +16,20 @@ class SQLParser
     private TokenizerInterface $tokenizer;
     private AstBuilderInterface $astBuilder;
     private SqlBuilderInterface $sqlBuilder;
+    private ModifierReplacerInterface $modifierReplacer;
 
     public function __construct(
         CommentRemoverInterface $commentRemover,
         TokenizerInterface $tokenizer,
         AstBuilderInterface $astBuilder,
-        SqlBuilderInterface $sqlBuilder
+        SqlBuilderInterface $sqlBuilder,
+        ModifierReplacerInterface $modifierReplacer
     ) {
         $this->commentRemover = $commentRemover;
         $this->tokenizer = $tokenizer;
         $this->astBuilder = $astBuilder;
         $this->sqlBuilder = $sqlBuilder;
+        $this->modifierReplacer = $modifierReplacer;
     }
 
     public function parse(string $query): array
@@ -38,5 +44,10 @@ class SQLParser
     public function toSQL(array $ast): string
     {
         return $this->sqlBuilder->buildSql($ast);
+    }
+
+    public function replaceModifiers(array $ast, array $modifiers, mysqli $mysqli): array
+    {
+        return $this->modifierReplacer->replace($ast, $modifiers, $mysqli);
     }
 }
